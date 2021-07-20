335461a97cea7903a146db4c1a6b4fda15ed380d
Soha Jin <soha@lohu.info>
2021-07-20 17:39:49 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 const { TaskGenerate } = require('gulp-task-static-pages');
 
 Object.assign(exports, TaskGenerate({ 
-	isProduction: process.env.NODE_ENV === 'production' || (process.argv.indexOf('--prod') > -1),
-	twigPath: './templates',
-	fontPath: './fonts',
-	fontDestinationDirectory: 'fonts'
+	isProduction: process.env.NODE_ENV === 'production' || (process.argv.indexOf('--prod') > -1)
  }));
